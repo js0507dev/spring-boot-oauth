30fd4fde6affff4ae77f7dda8ce8071cafcad750
Jeongseop Shim <js0507dev@gmail.com>
2018-04-20 13:53:03 +0900

    첫번째 commit

---
+++ backend/src/main/java/com/jeongseop/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.jeongseop.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping("/")
+	public String index(HttpServletRequest request) {
+		return "hello!";
+	}
+}
