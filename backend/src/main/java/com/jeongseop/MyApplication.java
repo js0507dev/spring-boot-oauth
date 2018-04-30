74b4e3c02a9eeffde8fd979a0e080e16628f1882
Jeongseop Shim <js0507dev@gmail.com>
2018-04-30 14:50:46 +0900

    Oauth2 JdbcTokenStore 설정

--- backend/src/main/java/com/jeongseop/MyApplication.java
+++ backend/src/main/java/com/jeongseop/MyApplication.java
@@ -2,21 +2,36 @@ package com.jeongseop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @EnableResourceServer
 @EnableAuthorizationServer
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
 
+	@Override
 	public void configure(HttpSecurity http) throws Exception {
+        http.headers()
+                .frameOptions().disable();
 	    http.authorizeRequests()
-                .antMatchers("/api/**").authenticated();
+                .anyRequest().permitAll()
+                .antMatchers("/members/**").authenticated();
 	}
+
+	@Bean
+    public TokenStore jdbcTokenStore(DataSource dataSource) {
+	    return new JdbcTokenStore(dataSource);
+    }
 }
