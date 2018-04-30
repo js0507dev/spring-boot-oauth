d8cb28a266c49cd65d8cdff7a090280d51581578
Jeongseop Shim <js0507dev@gmail.com>
2018-04-30 14:11:32 +0900

    oauth2 최초 셋팅

--- backend/src/main/java/com/jeongseop/MyApplication.java
+++ backend/src/main/java/com/jeongseop/MyApplication.java
@@ -2,11 +2,21 @@ package com.jeongseop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 public class MyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+	}
+
+	public void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+                .antMatchers("/api/**").authenticated();
 	}
 }
