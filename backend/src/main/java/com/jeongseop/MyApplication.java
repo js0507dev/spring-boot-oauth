f1e7b7ef3cf79f421ba25d97a9d0cdba5569b6e8
Jeongseop Shim <js0507dev@gmail.com>
2018-05-09 17:54:33 +0900

    Spring Security에서 Oauth2.0으로 변경작업중

--- backend/src/main/java/com/jeongseop/MyApplication.java
+++ backend/src/main/java/com/jeongseop/MyApplication.java
@@ -1,12 +1,22 @@
 package com.jeongseop;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @SpringBootApplication
 public class MyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+	}
+	
+	@Bean
+	public TokenStore JdbcTokenStore(DataSource dataSource) {
+		return new JdbcTokenStore(dataSource);
 	}
 }
